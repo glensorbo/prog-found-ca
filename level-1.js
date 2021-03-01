ace11c03e0965ca100a8c32a45d1ebc92bd80cd7
glensorbo <pookiecode@gmail.com>
2021-03-01 09:56:21 +0000

    Level 1 - Q2

--- level-1.js
+++ level-1.js
@@ -4,3 +4,13 @@
  */
 
 const imageURL = "https://images.miabelle.tv/large/Kickitalt_cover_generic.jpg";
+
+/*
+ * 2.
+ * Create an object variable called person and give it two properties (a key and a value) of your choice.
+ */
+
+const user = {
+  email: "email@email.com",
+  password: "aGreatPasswordToBeHashed",
+};
