ad9d46ff08eb80ceeee26a6e4f99097163b2c0ac
glensorbo <pookiecode@gmail.com>
2021-03-01 09:56:42 +0000

    Level 1 - Q3

--- level-1.js
+++ level-1.js
@@ -14,3 +14,23 @@ const user = {
   email: "email@email.com",
   password: "aGreatPasswordToBeHashed",
 };
+
+/*
+ * 3.
+ * Create a variable called outOfStock and assign it a boolean value.
+ * Create an if else statement that checks the value of outOfStock.
+ * If it is true, console log "Out of stock". Otherwise log "In stock".
+ */
+
+const outOfStock = true;
+
+if (outOfStock === true) {
+  console.log("Q3: " + "Out of stock");
+} else {
+  console.log("Q3: " + "In stock");
+}
+
+// Had to try ternary also
+outOfStock === true
+  ? console.log("Q3: " + "Out of stock")
+  : console.log("Q3: " + "In stock");
