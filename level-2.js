1d183d6607b91e0eee5ab78c795c47162d625285
glensorbo <pookiecode@gmail.com>
2021-03-01 09:58:55 +0000

    Level 2 - Q2

--- level-2.js
+++ level-2.js
@@ -12,3 +12,24 @@ for (let i = 15; i <= 25; i++) {
     console.log("Q1", i);
   }
 }
+
+/*
+ * 2.
+ * Create a function that logs the sentence "I am a function".
+ * Assign this function to a variable called innerFunction.
+ * Create another function called outerFunction that accepts one argument.
+ * Inside outerFunction, call the argument like you would a function - with parenthesis ().
+ * Call outerFunction and pass in the innerFunction variable.
+ */
+
+function consoleLog() {
+  console.log("I am a function");
+}
+
+const innerFunction = consoleLog;
+
+function outerFunction(cb) {
+  cb();
+}
+
+outerFunction(innerFunction);
